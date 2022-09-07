c6bc9c3b6fa80e77c37ad3cade5c4cff65fb8e99
RomanSchmid <rrr.schmid@gmail.com>
2022-09-07 23:22:23 +0200

    Regex update

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ const passwordError = document.querySelector(".password-error");
 const confirmPasswordError = document.querySelector(".confirm-password-error");
 const emailError = document.querySelector(".email-error");
 
-const emailRegex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+const usernameRegex = new RegExp(".{4,64}$");
+const passwordRegex = new RegExp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[#?!@$%^&*-]).{8,64}$");
+const emailRegex = new RegExp("[a-z0-9]+@[a-z]+\.[a-z]{2,3}");
 
 const usernameValidation = (username) => {
-    if (username.length >= 4 && username.length <= 64) {
+    if (usernameRegex.test(username)) {
         usernameError.style.display = "none";
         return true;
     } else {
@@ -19,7 +21,7 @@ const usernameValidation = (username) => {
 }
 
 const passwordValidation = (password) => {
-    if (password.length >= 8 && password.length <= 64) {
+    if (passwordRegex.test(password)) {
         passwordError.style.display = "none";
         return true;
     } else {
@@ -48,22 +50,21 @@ const emailValidation = (email) => {
 form.addEventListener("submit", (event) => {
     // Turn off page refreshing after every form submiting
     event.preventDefault();
-    console.log(event);
+
+    /* console.log(event); */
+
     let username = event.target.elements.username.value;
     let password = event.target.elements.password.value;
     let confirmPassword = event.target.elements.confirmPassword.value;
     let email = event.target.elements.email.value;
     let date = event.target.elements.date.value;
-    /* console.log(username);
-    console.log(password);
-    console.log(confirmPassword);
-    console.log(email);
-    console.log(date); */
+ 
     let cond1 = usernameValidation(username);
     let cond2 = passwordValidation(password);
     let cond3 = confirmPasswordValidation(password, confirmPassword);
     let cond4 = emailValidation(email);
 
+    // If all conditions are true -> Add an user to the user list
     if (cond1 && cond2 && cond3 && cond4) {
 
         userList.removeChild(empty);
